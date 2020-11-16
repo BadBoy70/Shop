b39b04b2dd36c2ba65dba69b930dfee408f77692
morteza.alizadeh70 <morteza.alizadeh70@outlook.com>
2020-11-16 15:46:21 +0330

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //       var host = CreateHostBuilder(args).Build();
+    //        using (var scope = host.Services.CreateScope())
+    //        {
+    //            var services = scope.ServiceProvider;
+    //            try
+    //            {
+    //                var context = services.GetRequiredService<DataContext>();
+    //                context.Database.Migrate();
+    //            }
+    //            catch (Exception ex)
+    //            {
+    //                var logger = services.GetRequiredService<ILogger<Program>>();
+    //                logger.LogError(ex, "An erorr occured during migration");
+
+    //            }
+    //        }
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
+    //}
+}
