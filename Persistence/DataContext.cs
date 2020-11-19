0766c8e988b804231aac89c06244d3bad480015f
morteza.alizadeh70 <morteza.alizadeh70@outlook.com>
2020-11-19 13:45:18 +0330

    test s

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,8 @@ namespace Persistence
 
         }
         public DbSet<Value> Values { set; get; }
+        public DbSet<Activity> Activities { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
